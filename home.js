0e78aa1efcc7488f55176d0751d4c84e33e8ce89
sakibHasan7439 <sakibhasanb120@gmail.com>
2024-09-19 00:08:51 +0600

    Home and login section added

---
+++ home.js
@@ -0,0 +1,3 @@
+const addMoneyBtn = document.getElementById("add-money");
+const pinNumber = document.getElementById("pin-Number");
+const addAmount = document.getElementById("add-amount");
---
+++ index.js
@@ -0,0 +1,20 @@
+const loginBtn = document.getElementById("login-button");
+const numberField = document.getElementById("number-input");
+const password = document.getElementById("password");
+
+loginBtn.addEventListener("click", (event)=>{
+    event.preventDefault();
+    validateForm();
+});
+
+const validateForm = function(){
+    // this way to validate number or password is wrong
+    if(numberField.value.length === 11 && password.value === "1234"){
+        console.log("You are logged in");
+        window.location.href = "home.html";
+
+    }else {
+        alert("Ja vag chor");
+    }
+
+}
